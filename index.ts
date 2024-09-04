b94540a69c878c59299158a921c59659e7cb7d62
akika <you@example.com>
2024-09-04 10:06:36 +0200

    bdbs

--- index.ts
+++ index.ts
@@ -1,2 +1,23 @@
-const myName: string = "Tomi"
-console.log(myName)
+const numEl: HTMLInputElement = document.getElementById("inputNum") as HTMLInputElement
+
+const resultEl: HTMLParagraphElement = document.getElementById("result") as HTMLParagraphElement
+
+const theNumber = Math.floor(Math.random() * 100 ) + 1
+
+function guess(){
+    const guessNumber: number = numEl.valueAsNumber
+    if(isCorrect(guessNumber)){
+        resultEl.innerText = "Eltaláltad!"
+    }else{
+        if (guessNumber < theNumber) {
+            resultEl.innerText = "A gondolt szám nagyobb!"
+        }
+        else{
+            resultEl.innerText = "A gondolt szám kisebb!"
+        }
+    }
+}
+
+function isCorrect(guessNumber: number) : boolean {
+    return guessNumber === theNumber
+}
